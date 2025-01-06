f23c819dd7435b04c701d9a14bdc0c009df27ee1
AfonsoMmsc <afonsonogueirammsc@gmail.com>
2025-01-06 13:04:26 +0000

    base backend

---
+++ sofico-backend/src/main/java/com/sofico/sofico_backend/SoficoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sofico.sofico_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoficoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoficoBackendApplication.class, args);
+	}
+
+}
---
+++ sofico-backend/src/test/java/com/sofico/sofico_backend/SoficoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofico.sofico_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoficoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
